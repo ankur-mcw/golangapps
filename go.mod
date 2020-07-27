0fac01367d9ee0b0f774c5f8f0237daa4e62fba3
Ankur Marchattiwar <ankur.marchattiwar>
2020-07-28 00:28:33 +0530

    added dependencies from narvar/NarvarGolangApps

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/ankur-mcw/golangapps
+
+go 1.13
+
+require (
+	github.com/buaazp/fasthttprouter v0.1.1
+	github.com/narvar/NarvarGolangApps v0.0.8
+	github.com/valyala/fasthttp v1.15.1
+)
--- services/labelGeneration/processor/createLabel.go
+++ services/labelGeneration/processor/createLabel.go
@@ -2,7 +2,7 @@ package processor
 
 import (
 	"errors"
-	"log"
+	"github.com/narvar/NarvarGolangApps/nlog"
 )
 
 type LabelGenerationRequest struct {
@@ -14,6 +14,7 @@ type LabelGenerationResponse struct {
     Message string `json:"message"`
 }
 
+// GenerateLabel for the given carrier
 func GenerateLabel(request LabelGenerationRequest) (*LabelGenerationResponse, error) {
 
     response := &LabelGenerationResponse{}
@@ -21,12 +22,12 @@ func GenerateLabel(request LabelGenerationRequest) (*LabelGenerationResponse, er
     if len(carrierMoniker) == 0 {
         return response, errors.New("Carrier moniker cannot be empty")
     }
-    log.Printf("CarrierMoniker=%s", carrierMoniker)
+    nlog.Debugf("CarrierMoniker=%s", carrierMoniker)
 
     retailerMoniker := request.RetailerMoniker
     if len(retailerMoniker) == 0 {
         return response, errors.New("Retailer moniker cannot be empty")
     }
-    log.Printf("RetailerMoniker=%s", retailerMoniker)
+    nlog.Debugf("RetailerMoniker=%s", retailerMoniker)
     return &LabelGenerationResponse{"created"}, nil
 }
